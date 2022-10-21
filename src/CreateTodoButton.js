eb8b682a56a7ceff729add27739e1af5a30c2141
Karla Sequen <karlasequen@gmail.com>
2022-10-21 14:33:30 -0600

    add components to app

---
+++ src/CreateTodoButton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './CreateTodoButton.css';
+
+function CreateTodoButton() {
+    return (
+        <button             
+            className='CreateTodoButton'
+        >
+                
+            +
+        
+        </button>
+    );
+}
+
+export { CreateTodoButton };
---
+++ src/TodoCounter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './TodoCounter.css';
+
+function TodoCounter() {
+    return (
+        <h2 className='TodoCounter'>You have completed 2 of 3 TODOs</h2>
+    );
+}
+
+export { TodoCounter };
